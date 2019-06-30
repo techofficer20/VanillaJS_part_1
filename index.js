fc54b0f97f40c8010d6b0b58d5f5447aa8f60365
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-01 02:06:29 +0900

    2.1.1 More Function Fun

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function sayHello(name, age) {
-  console.log("Hello!", name, "you have", age, "years of age.");
-}
-sayHello("Nicolas", 15);
+const calculator = {
+  plus: function(a, b) {
+    return a + b;
+  }
+};
+const plus = calculator.plus(5, 5);
+console.log(plus);
