ae179bc8c01c1df8b896bb81c716ff874a7b0023
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-06-30 17:47:31 +0900

    1.10 Organizing Data with Objects

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const something = "Something";
-const daysOfWeek = [
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat",
-  "Sun",
-  54,
-  true,
-  "stuff",
-  "lalala",
-  something
-];
-
-console.log(daysOfWeek);
+const nicoInfo = {
+  name: "Nico",
+  age: 33,
+  gender: "Male",
+  isHandsome: true,
+  // Object 안에 Array 넣기 가능
+  favMovies: ["Along the Gods", "LOTR", "Oldboy"],
+  // Array 안에 Object 넣기 가능
+  favFood: [
+    { name: "Kimchi", fatty: false },
+    { name: "Cheese burger", fatty: true }
+  ]
+};
+console.log(nicoInfo.favFood[0].fatty);
