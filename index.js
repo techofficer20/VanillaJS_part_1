29060f954f2fb3c85df9bd8727cd5a63686e9916
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-06-30 17:15:41 +0900

    1.8 Data Types on JS

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const a = 221;
-let b = a - 5;
-// a = 4; const로 선언했을 시 오류 발생.
-console.log(b, a);
-/* let과 var의 차이
-let: 같은 변수를 다시 선언했을 때 오류 발생
-var: 같은 변수를 다시 선언했을 때 오류 X
-그러므로 보다 완벽한 코드를 위해 let을 쓸 것.
+/* String
+const what = "Nicolas";
+*/
+
+/* Boolean
+const wat = true; (true = 1, false = 0)
+*/
+
+/* Number
+const wat = 666;
+*/
+
+/* Float
+const wat = 55.1;
 */
